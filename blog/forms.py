9a997dfa4b799845867d1b910e5ecaca3843713e
Alex-Spo <spoleducacion@gmail.com>
2023-11-21 10:51:08 +0100

    Pagination

---
+++ blog/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+from blog.models import Comment
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['body']
+
+        widgets = {
+            'body': forms.Textarea(attrs={
+                'placeholder': 'Share your thoughts in the comments...',
+                'rows': 4
+            })
+        }
+
+        labels = {
+            'body': 'Add your comment'
+        }
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from blog import models
@@ -5,4 +6,10 @@ from blog import models
 
 def post_list_view(request):
     posts = models.Post.objects.all()
-    return render(request, 'blog/post/post_list.html', {'posts': posts})
+
+    paginator = Paginator(posts, 4)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'blog/post/post_list.html', {'page_obj': page_obj, 'posts': posts})
+
